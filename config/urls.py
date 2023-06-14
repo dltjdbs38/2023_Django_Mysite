1ace7745034801450e2e7fa726de4ca240aaae42
dltjdbs38 <aassdd38@naver.com>
2023-06-14 10:09:45 +0900

    pybo views.py, config urls.py 수정

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from pybo import views # 추가
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("pybo/", views.index),  # 추가 views.py의 index 함수를 사용하겠다.
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse # 추가
 # Create your views here.
+
+def index(request): # 추가 - 실제 사용자에게 보여지는 공간. request는 clien의 요청
+    return HttpResponse("pybo에서 응답해줄 내용")
+
