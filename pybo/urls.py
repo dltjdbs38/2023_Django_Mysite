7bc39ba23ffa048561781b1c1dc4618201d0b719
dltjdbs38 <aassdd38@naver.com>
2023-06-15 13:34:35 +0900

    views에 app_name과 url pybo:detail 로 별칭&네임스페이스

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,8 +2,10 @@
 from django.urls import path # 경로를 이용하기 위한 
 from . import views # . 은 위치 
 
+app_name = 'pybo'
+
 urlpatterns = [ # 이미 :8000/pybo/로 들어옴
-    path("", views.index),  
-    path("<int:q_id>/", views.detail), # pybo/1 로 가도록 q_id라는 내 URL 변수를 만든 것이다. 
+    path("", views.index, name='index'),  
+    path("<int:q_id>/", views.detail, name='detail'), # pybo/1 로 가도록 q_id라는 내 URL 변수를 만든 것이다. 
     # 그리고 views.py 의 detail 함수에 연결해 줄 것이다.
 ]
