24c096a12040710a2734b7ce7c4eee7440d447ff
dltjdbs38 <aassdd38@naver.com>
2023-06-15 11:06:54 +0900

    Django html 주석은 {% comment %} {% endcomment %} 로 한다.

--- pybo/views.py
+++ pybo/views.py
@@ -9,7 +9,7 @@ from pybo.models import Question
 
 # 우리는 오늘 DB와 연동한 template를 사용자에게 보여줄 것이다.
 def index(request): # 사용자로부터 인자를 받자(요청 = request)
-    question_list = Question.objects.order_by(-'create_date') # DB의 objects를 가져와서 시간순 정렬
+    question_list = Question.objects.order_by('-create_date') # DB의 objects를 가져와서 시간순 정렬
     # order_by default는 내림차순, 우리는 최근부터 보여줘야하니까 오름차순으로 -
     context = {'question_list' : question_list} # dict로 포장해서 사용자에게 보내주기
     return render(request, 'pybo/question_list.html', context) # 요청, template 경로, 보여줄내용
